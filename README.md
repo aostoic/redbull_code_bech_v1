ccf7222ac6de39b310ce53c6efa773159045c481
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-11-09 17:50:08 -0300

    Flutter hooks

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 1. [Flutter launcher icons](https://pub.dev/packages/flutter_launcher_icons)
 2. [Rive](https://pub.dev/packages/rive)
+3. [Flutter hooks](https://pub.dev/packages/flutter_hooks)
 
 ## Developers
 
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:redbull_code_bech_v1/helpers/helpers.dart';
 import 'package:redbull_code_bech_v1/pages/pages.dart';
 import 'package:rive/rive.dart';
 
-class LoadingPage extends StatelessWidget {
+class LoadingPage extends HookWidget {
   static String routeName = '/loading';
 
   const LoadingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Future checkAuth() async {
+    useEffect(() {
       Future.delayed(const Duration(milliseconds: 1000), () {
         Navigator.pushReplacement(
           context,
@@ -20,19 +21,17 @@ class LoadingPage extends StatelessWidget {
           ),
         );
       });
-    }
+    }, []);
 
     return Scaffold(
       backgroundColor: AppColors.primaryDarkColor,
-      body: FutureBuilder(
-          future: checkAuth(),
-          builder: (_, __) {
-            return Container(
-              color: AppColors.primaryDarkColor,
-              padding: const EdgeInsets.all(50),
-              child: const RiveAnimation.asset('assets/rives/loader.riv'),
-            );
-          }),
+      body: Container(
+        color: AppColors.primaryDarkColor,
+        padding: const EdgeInsets.all(50),
+        child: const RiveAnimation.asset(
+          'assets/rives/loader.riv',
+        ),
+      ),
     );
   }
 }
