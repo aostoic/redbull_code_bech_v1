1a5d43d65b3c7b098f5992689ced0baf3a63191d
Andro Ostoic <androstoic@gmail.com>
2021-11-11 16:14:14 -0300

    feat funciones para ingresar con correo

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,24 +1,88 @@
-// ignore_for_file: import_of_legacy_library_into_null_safe
-
 import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/foundation.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthenticationService {
-  // static Future<UserCredential> signInWithGoogle() async {
-  // Trigger the authentication flow
-  // final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  // static Future<User?> signInWithGoogle() async {
+  //   FirebaseAuth _auth = FirebaseAuth.instance;
+  //   try {
+  //     UserCredential userCredential;
 
-  // // Obtain the auth details from the request
-  // final GoogleSignInAuthentication? googleAuth =
-  //     await googleUser?.authentication;
+  //     if (kIsWeb) {
+  //       var googleProvider = GoogleAuthProvider();
+  //       userCredential = await _auth.signInWithPopup(googleProvider);
+  //     } else {
+  //       final GoogleSignInAccount googleUser = (await GoogleSignIn().signIn())!;
+  //       final GoogleSignInAuthentication googleAuth =
+  //           await googleUser.authentication;
+  //       final googleAuthCredential = GoogleAuthProvider.credential(
+  //         accessToken: googleAuth.accessToken,
+  //         idToken: googleAuth.idToken,
+  //       );
+  //       userCredential = await _auth.signInWithCredential(googleAuthCredential);
+  //     }
 
-  // // Create a new credential
-  // final credential = GoogleAuthProvider.credential(
-  //   accessToken: googleAuth?.accessToken,
-  //   idToken: googleAuth?.idToken,
-  // );
-
-  // // Once signed in, return the UserCredential
-  // return await FirebaseAuth.instance.signInWithCredential(credential);
+  //     final user = userCredential.user;
+  //     return user;
+  //   } catch (e) {
+  //     print(e);
+  //   }
   // }
+
+  // static Future<UserCredential> signInWithGoogle() async {
+  //   // Trigger the authentication flow
+  //   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+  //   // Obtain the auth details from the request
+  //   final GoogleSignInAuthentication? googleAuth =
+  //       await googleUser?.authentication;
+
+  //   // Create a new credential
+  //   final credential = GoogleAuthProvider.credential(
+  //     accessToken: googleAuth?.accessToken,
+  //     idToken: googleAuth?.idToken,
+  //   );
+
+  //   // Once signed in, return the UserCredential
+  //   return await FirebaseAuth.instance.signInWithCredential(credential);
+  // }
+
+  static Future<User?> createUserWithEmailAndPassword(email, password) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      final user = userCredential.user;
+      return user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  static Future<User?> signInWithEmailAndPassword(email, password) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+
+      final user = userCredential.user;
+      return user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  static Future<String> signOut() async {
+    await FirebaseAuth.instance.signOut();
+    return "signOut";
+  }
 }
--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,1 +1,1 @@
-// export 'package:redbull_code_bech_v1/services/auth.dart';
+export 'package:redbull_code_bech_v1/services/auth.dart';
