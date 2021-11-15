0fbc533b819edb5ef00614e066c6b2034ec45822
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-11-15 15:24:05 -0300

    save

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -9,9 +9,12 @@ import 'package:google_sign_in/google_sign_in.dart';
 String generateNonce([int length = 32]) {
   const charset =
       '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+
   final random = Random.secure();
-  return List.generate(length, (_) => charset[random.nextInt(charset.length)])
-      .join();
+  return List.generate(
+    length,
+    (_) => charset[random.nextInt(charset.length)],
+  ).join();
 }
 
 String sha256ofString(String input) {
