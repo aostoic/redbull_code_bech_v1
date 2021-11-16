59e47e13054bda83d4a21215941ab94587412a20
Andro Ostoic <androstoic@gmail.com>
2021-11-15 23:57:57 -0300

    feat : algunos arreglos visuales

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:redbull_code_bech_v1/helpers/fade_in_navigation.dart';
+import 'package:redbull_code_bech_v1/pages/pages.dart';
+import 'package:redbull_code_bech_v1/services/auth.dart';
 
 class ProfilePage extends StatelessWidget {
   static String routeName = '/profile';
@@ -8,14 +12,27 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context);
+
+    void handleLogout() async {
+      await authService.logout();
+
+      Navigator.of(context).pushReplacement(
+        fadeInNavigation(
+          context,
+          const SignInPage(),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Perfil'),
         actions: <Widget>[
           Padding(
-              padding: EdgeInsets.only(right: 20.0),
+              padding: const EdgeInsets.only(right: 20.0),
               child: GestureDetector(
-                onTap: () {},
+                onTap: () => handleLogout(),
                 child: const Icon(FontAwesomeIcons.signOutAlt),
               )),
         ],
