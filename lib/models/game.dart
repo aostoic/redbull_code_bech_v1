25b32dc027a9251b0f157b550d68fe18a3a7ea11
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-11-19 15:40:01 -0300

    avances modelos

--- lib/models/game.dart
+++ lib/models/game.dart
@@ -1,1 +1,11 @@
-class Game {}
+class Game {
+  String id;
+  String name;
+  String urlImage;
+
+  Game({
+    required this.id,
+    required this.name,
+    required this.urlImage,
+  });
+}
--- lib/models/tournament.dart
+++ lib/models/tournament.dart
@@ -1,1 +1,19 @@
-class Tournament {}
+class Tournament {
+  String id;
+  String title;
+  String description;
+  String status;
+  String gameId;
+  String tournamentType;
+  String urlImage;
+
+  Tournament({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.status,
+    required this.gameId,
+    required this.tournamentType,
+    required this.urlImage,
+  });
+}
