15557f74d67abbad28deb3461bc4b68f13987bce
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-11-19 15:10:36 -0300

    save

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -56,7 +56,7 @@ class ProfileHeader extends StatelessWidget {
     final authService = Provider.of<AuthService>(context);
 
     return Container(
-      height: size.height * 0.3,
+      height: size.height * 0.4,
       width: double.infinity,
       decoration: const BoxDecoration(
         color: Colors.white,
@@ -81,99 +81,217 @@ class ProfileHeader extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Partidas',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'Ubuntu',
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(width: 10),
-              Icon(
-                FontAwesomeIcons.gamepad,
-                color: Colors.black,
-              )
-            ],
-          ),
-          const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                children: const [
-                  Text(
-                    '0',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Ubuntu',
-                      fontSize: 15,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  Text(
-                    'Jugadas',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Ubuntu',
-                      fontSize: 15,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: const [
-                  Text(
-                    '0',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Ubuntu',
-                      fontSize: 15,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  Text(
-                    'Ganadas',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Ubuntu',
-                      fontSize: 15,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: const [
-                  Text(
-                    '0',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Ubuntu',
-                      fontSize: 15,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  Text(
-                    'Perdidas',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Ubuntu',
-                      fontSize: 15,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+          const TournamentsPartHeader(),
+          const SizedBox(height: 20),
+          const GamesPartHeader(),
         ],
       ),
+    );
+  }
+}
+
+class TournamentsPartHeader extends StatelessWidget {
+  const TournamentsPartHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'Torneos',
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Ubuntu',
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(width: 10),
+            Icon(
+              FontAwesomeIcons.trophy,
+              color: Colors.black,
+            )
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: const [
+                Text(
+                  '0',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Ubuntu',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  'Jugados',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Ubuntu',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: const [
+                Text(
+                  '0',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Ubuntu',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  'Ganados',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Ubuntu',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: const [
+                Text(
+                  '0',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Ubuntu',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  'Perdidos',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Ubuntu',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class GamesPartHeader extends StatelessWidget {
+  const GamesPartHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'Partidas',
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Ubuntu',
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(width: 10),
+            Icon(
+              FontAwesomeIcons.gamepad,
+              color: Colors.black,
+            )
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: const [
+                Text(
+                  '0',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Ubuntu',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  'Jugadas',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Ubuntu',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: const [
+                Text(
+                  '0',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Ubuntu',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  'Ganadas',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Ubuntu',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: const [
+                Text(
+                  '0',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Ubuntu',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  'Perdidas',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Ubuntu',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
