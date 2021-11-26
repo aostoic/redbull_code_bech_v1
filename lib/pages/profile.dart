7bbce43585d3cf380d080f9b6ce6a424ab584123
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-11-26 10:27:45 -0300

    save

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,9 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:redbull_code_bech_v1/helpers/fade_in_navigation.dart';
 import 'package:redbull_code_bech_v1/helpers/helpers.dart';
 import 'package:redbull_code_bech_v1/pages/pages.dart';
-import 'package:redbull_code_bech_v1/services/auth.dart';
 import 'package:redbull_code_bech_v1/services/services.dart';
 import 'package:redbull_code_bech_v1/widgets/widgets.dart';
 
@@ -14,6 +15,39 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mi Perfil'),
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            PlayerHeader(player: authService.user!),
+            const SizedBox(height: 10),
+            PlayerProfileOptions(player: authService.user!),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PlayerProfileOptions extends StatelessWidget {
+  final User player;
+
+  const PlayerProfileOptions({
+    Key? key,
+    required this.player,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     final navigationService = Provider.of<NavigationService>(context);
     final authService = Provider.of<AuthService>(context);
 
@@ -32,19 +66,48 @@ class ProfilePage extends StatelessWidget {
       });
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Mi Perfil'),
+    return Container(
+      width: double.infinity,
+      height: size.height * 0.15,
+      decoration: const BoxDecoration(
+        color: Colors.white,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Column(
         children: [
-          PlayerHeader(player: authService.user!),
-          Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: PrimaryButton(
-              text: 'Cerrar Sesión',
-              onPressed: () => handleLogout(),
+          const ListTile(
+            leading: Icon(
+              FontAwesomeIcons.trophy,
+              color: AppColors.backgroundDarkColor,
+            ),
+            title: Text(
+              'Mis torneos',
+              style: TextStyle(
+                color: AppColors.backgroundDarkColor,
+                fontFamily: 'Ubuntu',
+              ),
+            ),
+            trailing: Icon(
+              FontAwesomeIcons.arrowCircleRight,
+              color: AppColors.backgroundDarkColor,
+            ),
+          ),
+          const Divider(color: AppColors.backgroundDarkColor),
+          ListTile(
+            onTap: () => handleLogout(),
+            leading: const Icon(
+              FontAwesomeIcons.signOutAlt,
+              color: AppColors.backgroundDarkColor,
+            ),
+            title: const Text(
+              'Cerrar Sesión',
+              style: TextStyle(
+                color: AppColors.backgroundDarkColor,
+                fontFamily: 'Ubuntu',
+              ),
+            ),
+            trailing: const Icon(
+              FontAwesomeIcons.arrowCircleRight,
+              color: AppColors.backgroundDarkColor,
             ),
           ),
         ],
