a3928845e7ae97aaaef6450ab379282ef37fb373
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-11-30 17:39:37 -0300

    save

--- lib/pages/create_tournament.dart
+++ lib/pages/create_tournament.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:redbull_code_bech_v1/forms/forms.dart';
 
 class CreateTournamentPage extends StatelessWidget {
   static String routeName = '/create-tournament';
@@ -11,6 +13,19 @@ class CreateTournamentPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Crear torneo'),
       ),
+      body: ChangeNotifierProvider(
+        create: (BuildContext context) => CreateTournamentForm(),
+        child: const _CreateForm(),
+      ),
     );
+  }
+}
+
+class _CreateForm extends StatelessWidget {
+  const _CreateForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
