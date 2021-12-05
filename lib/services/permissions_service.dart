938f77d248bd74b8bb79403370f5a13866bcc164
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-12-05 14:58:19 -0300

    avances permissions service

---
+++ lib/services/permissions_service.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class PermissionsService with ChangeNotifier {
+  Future<void> getGalleryPermissions() async {}
+}
--- lib/services/services.dart
+++ lib/services/services.dart
@@ -2,4 +2,5 @@ export 'package:redbull_code_bech_v1/services/auth.dart';
 export 'package:redbull_code_bech_v1/services/categories.dart';
 export 'package:redbull_code_bech_v1/services/firebase.dart';
 export 'package:redbull_code_bech_v1/services/navigation.dart';
+export 'package:redbull_code_bech_v1/services/permissions_service.dart';
 export 'package:redbull_code_bech_v1/services/tournament.dart';
